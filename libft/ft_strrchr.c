63d4dcd79d0079d97bb6125ecc8688991957bf8c
Youssef Fouzi <yofouzi@e1r5p9.1337.ma>
2024-10-30 01:22:43 +0100

    I'm working don't push too hard

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yofouzi <yofouzi@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 00:45:15 by yofouzi           #+#    #+#             */
+/*   Updated: 2024/10/30 01:13:50 by yofouzi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+    unsigned int i;
+    char cc;
+    const char *last;
+
+    last = NULL;
+    i = 0;
+    cc = (char)c;
+    while (s[i])
+    {
+        if (s[i] == cc)
+            last = &s[i];
+        i++;
+    }
+    if (cc == '\0')
+        return ((char *)&s[i]);
+    return ((char *)last);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -32,5 +32,7 @@ void    *ft_memset(void *b, int c, size_t len);
 void    *ft_memcpy(void *dest, const void *src, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
+char    *ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
